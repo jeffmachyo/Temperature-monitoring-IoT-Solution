3b9bd9ac05a744aaed72b58f1dc999207785e275
jeffmachyo <jeffmachyo@gmail.com>
2022-03-13 12:23:32 -0400

    Update main.c

--- main.c
+++ main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "stdio.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
